5d6a9627784747f1022df6040ca32a96eaa1d8d9
flwwsg <flwwsg@qq.com>
2020-12-20 17:39:03 +0800

    add index.html

--- bookmarker/app/main.js
+++ bookmarker/app/main.js
@@ -1,6 +1,11 @@
-// node process, does not have DOM, and can not render a UI
-const { app, BrowserView } = require('electron');
+// node process, does not have DOM, and can not render a UI.
+// main process 用于跟系统交互，调用底层，管理 render process，不过并不渲染 HTML、CSS
+const { app, BrowserWindow } = require('electron');
 
+// 创建 render process
+let mainWindow = null;
 app.on('ready', () => {
    console.log('hello electron');
+   mainWindow = new BrowserWindow();
+   mainWindow.webContents.loadFile(`${__dirname}/index.html`).then(r => {console.log('html loaded:', r)}).catch(r => {console.log('get error', r)});
 });
